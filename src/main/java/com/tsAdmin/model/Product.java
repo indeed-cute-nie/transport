9dcb44d6446537590093299c78b7b0305c24384b
indeed-cute-nie <166818180+indeed-cute-nie@users.noreply.github.com>
2025-03-03 16:48:22 +0800

    Update Product.java

--- src/main/java/com/tsAdmin/model/Product.java
+++ src/main/java/com/tsAdmin/model/Product.java
@@ -1,8 +1,22 @@
 package com.tsAdmin.model;
 
+import java.util.Random;
+
 /** 产品 */
 public class Product
 {
+	private static final Random random = new Random();
+	/** 
+	 * 货物类型 
+	 */
+	public static enum GoodsType
+	{
+	    WOOD,           // 木材
+	    STEEL,          // 钢材
+	    PHARMACEUTICAL  // 药材
+	}
+	
+	
     private GoodsType type;
     private int quantity;
 
@@ -18,4 +32,26 @@ public class Product
     // Getter
     public GoodsType getType() { return type; }
     public int getQuantity() { return quantity; }
+    
+    public static GoodsType getRandomProductType()//随机产品类型 
+    {
+        GoodsType[] types = GoodsType.values();
+        int index = random.nextInt(types.length);
+        return types[index];
+    }
+    
+    public static Product createProductByType(GoodsType type)//实例化产品 
+    {
+        switch (type) 
+        {
+            case WOOD:
+                return new Product(GoodsType.WOOD);
+            case STEEL:
+                return new Product(GoodsType.STEEL);
+            case PHARMACEUTICAL:
+                return new Product(GoodsType.PHARMACEUTICAL);
+            default:
+                throw new IllegalArgumentException("未知的生产厂类型: " + type);
+        }
+    }
 }
