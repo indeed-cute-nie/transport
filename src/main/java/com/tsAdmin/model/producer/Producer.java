827bbd6ed0df2a01ee6dd026b1720c71eef0dca0
indeed-cute-nie <166818180+indeed-cute-nie@users.noreply.github.com>
2025-03-03 16:49:01 +0800

    Update Producer.java

--- src/main/java/com/tsAdmin/model/producer/Producer.java
+++ src/main/java/com/tsAdmin/model/producer/Producer.java
@@ -2,12 +2,17 @@ package com.tsAdmin.model.producer;
 
 import java.util.Random;
 
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import com.tsAdmin.common.Coordinate;
 import com.tsAdmin.model.Demand;
 import com.tsAdmin.model.Product;
+import com.tsAdmin.model.Product.GoodsType;
 import com.tsAdmin.model.processor.Processor;
 
-/** 生产厂(抽象) */
+/** 
+ * 生产厂(抽象) 
+ */
 public abstract class Producer
 {
     protected String name;
@@ -18,7 +23,65 @@ public abstract class Producer
         this.name = name;
         this.position = position;
     }
+    
+    
+    public static Producer getProducer(GoodsType type) 
+    {
+        String tableName = getTableName(type);
+        if (tableName == null)
+        {
+            System.out.println("无效的类型");
+            return null;
+        }
+        String sql = "SELECT name, location_lat, location_lon FROM " + tableName + " ORDER BY RAND() LIMIT 1";
+        Record record = Db.findFirst(sql);
 
+        if (record != null) 
+        {
+            String name = record.getStr("name");
+            double locationLat = record.getDouble("location_lat");
+            double locationLon = record.getDouble("location_lon");
+            return createProducer(type, name, new Coordinate(locationLat, locationLon));
+        }
+        else 
+        {
+            System.out.println("数据库中没找到生产厂");
+            return null;
+        }
+    }
+    
+    
+    private static String getTableName(GoodsType type) 
+    {
+        switch (type) {
+            case WOOD:
+                return "wood_producer";
+            case STEEL:
+                return "steel_producer";
+            case PHARMACEUTICAL:
+            	return "pharmaceutical_producer";
+            default:
+                return null;
+        }
+    }
+    
+    
+    private static Producer createProducer(GoodsType type, String name, Coordinate coordinate) 
+    {
+        switch (type) 
+        {
+            case WOOD:
+                return new WoodProducer(name, coordinate);
+            case STEEL:
+                return new SteelProducer(name, coordinate);
+            case PHARMACEUTICAL:
+            	return new PharmaceuticalProducer(name, coordinate);
+            default:
+                return null;
+        }
+    }
+
+    
     public Demand produceDemand(Product product, Processor processor)
     {
         product.setQuantity(generateRandomQuantity());
@@ -30,7 +93,8 @@ public abstract class Producer
     }
     
 
-    private int generateRandomQuantity() {
+    private int generateRandomQuantity() 
+    {
         return new Random().nextInt(getMaxQuantity() - getMinQuantity() + 1) + getMinQuantity();
     }
 
