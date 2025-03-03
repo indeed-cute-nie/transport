fafb0332e4c0576a9dd78e2d23a598860e816bbd
indeed-cute-nie <166818180+indeed-cute-nie@users.noreply.github.com>
2025-03-03 16:46:45 +0800

    Update DemandSave.java

--- src/main/java/com/tsAdmin/model/DemandSave.java
+++ src/main/java/com/tsAdmin/model/DemandSave.java
@@ -6,29 +6,28 @@ import java.util.List;
 import com.jfinal.plugin.activerecord.Record;
 
 import com.tsAdmin.common.Coordinate;
-import com.tsAdmin.control.DataController;
 import com.tsAdmin.control.DBManager;
+import com.tsAdmin.model.Product.GoodsType;
 
 public class DemandSave
 {
 	public static ArrayList<Demand> demands = new ArrayList<Demand>();//在生成需求时添加订单到此数组
-    public void initDemands()
+    public static void initDemands()
     {
         // 检查表是否为空
-        if (!DataController.isTableEmpty("demand_table"))
+        if (!DBManager.isTableEmpty("demand"))
         {
             List<Record> records = DBManager.getDemands();
             for (Record record : records) 
             {
-                // 获取 ifremove 字段
-                int ifremove = record.getInt("ifremove");
                 // 跳过 ifremove = 1 的记录
+                int ifremove = record.getInt("ifremove");
                 if (ifremove == 1) continue;
                 // 获取其他字段
-                double origLat = record.getDouble("origin_la");
-                double origLon = record.getDouble("origin_lo");
-                double destLat = record.getDouble("destination_la");
-                double destLon = record.getDouble("destination_lo");
+                double origLat = record.getDouble("origin_lat");
+                double origLon = record.getDouble("origin_lon");
+                double destLat = record.getDouble("destination_lat");
+                double destLon = record.getDouble("destination_lon");
                 GoodsType type = GoodsType.valueOf(record.getStr("type"));
                 int quantity = record.getInt("quantity");
 
